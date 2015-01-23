458fd92f5ef0fc2fe6b0dc4f14d3a61a91af2c40
Emile ROBERT <erobert@e2r2p4.42.fr>
2015-01-23 13:30:26 +0100

    ???

---
+++ 42/fract'ol/hdezier/libft/ft_strmap.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdezier <hdezier@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/21 13:57:07 by hdezier           #+#    #+#             */
+/*   Updated: 2013/11/22 12:49:22 by hdezier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	char	*str;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	str = ft_strnew(ft_strlen(s));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(s[i]);
+		i++;
+	}
+	return (str);
+}
