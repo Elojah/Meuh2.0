4a047787cd25ac17d2266b2633df5ec1ff86aec2
Yyhrs <erobert@student.42.fr>
2015-02-24 15:38:05 +0100

    ft_sh2 done

---
+++ 42/ft_sh2/erobert/srcs/ft_pipe.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pipe.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erobert <erobert@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/01/15 12:43:49 by erobert           #+#    #+#             */
+/*   Updated: 2015/02/23 18:49:36 by erobert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <sys/wait.h>
+#include "ft_minishell2.h"
+
+static void	ft_father(int *pfd_1, char **tab, char ***env)
+{
+	pid_t	father;
+	int		pfd_2[2];
+	t_cmd	cmd;
+
+	close(pfd_1[1]);
+	dup2(pfd_1[0], 0);
+	close(pfd_1[0]);
+	if (!tab[1])
+	{
+		ft_exec(0, &cmd, *tab, env);
+		exit(0);
+	}
+	if (pipe(pfd_2) == -1)
+		return ;
+	father = fork();
+	if (!father)
+	{
+		close(pfd_2[0]);
+		dup2(pfd_2[1], 1);
+		close(pfd_2[1]);
+		ft_exec(0, &cmd, *tab, env);
+		exit(0);
+	}
+	else
+		ft_father(pfd_2, tab + 1, env);
+}
+
+void		ft_pipe(char *str, char ***env)
+{
+	char	**tab;
+	pid_t	father;
+	int		pfd[2];
+	t_cmd	cmd;
+
+	tab = ft_strsplit(str, '|');
+	father = fork();
+	if (father)
+		wait(0);
+	else
+	{
+		if (pipe(pfd) == -1)
+			return ;
+		father = fork();
+		if (!father)
+		{
+			close(pfd[0]);
+			dup2(pfd[1], 1);
+			close(pfd[1]);
+			ft_exec(0, &cmd, *tab, env);
+			exit(0);
+		}
+		else
+			ft_father(pfd, tab + 1, env);
+	}
+}
---
+++ 42/ft_sh2/erobert/srcs/ft_putprompt.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putprompt.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erobert <erobert@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/06 14:58:03 by erobert           #+#    #+#             */
+/*   Updated: 2015/02/23 18:49:28 by erobert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_minishell2.h"
+
+void		ft_putprompt(char **env)
+{
+	int		i;
+	int		j;
+
+	ft_putstr_fd("[", 1);
+	ft_putstr_fd(GREEN, 1);
+	ft_putstr_fd("1138", 1);
+	ft_putstr_fd(END, 1);
+	i = 0;
+	while (env[i] && ft_strncmp("PWD=", env[i], 4))
+		i++;
+	if (env[i])
+	{
+		j = ft_strlen(env[i] + 4);
+		while (env[i][j] != '/')
+			j--;
+		ft_putstr_fd(" ", 1);
+		ft_putstr_fd(YELLOW, 1);
+		ft_putstr_fd(env[i] + j + 1, 1);
+		ft_putstr_fd("/", 1);
+		ft_putstr_fd(END, 1);
+	}
+	ft_putstr_fd("] ", 1);
+}
