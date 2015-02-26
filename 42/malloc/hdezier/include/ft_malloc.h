0d6d72fc5db780027406e92d1932a71e33f363ce
Hugo DEZIER <hdezier@e3r4p18.42.fr>
2015-02-26 16:18:24 +0100

    ok

---
+++ 42/malloc/hdezier/include/ft_malloc.h
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_malloc.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdezier <hdezier@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/04 12:30:54 by hdezier           #+#    #+#             */
+/*   Updated: 2015/02/04 12:30:54 by hdezier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MALLOC_H
+# define FT_MALLOC_H
+
+# define TINY_PAGE 4
+# define SMALL_PAGE 16
+# define LARGE_PAGE 64
+
+# include <stdlib.h>
+
+typedef struct		s_mem
+{
+	size_t			size;
+	int				free;
+	struct s_mem	*next;
+}					t_mem;
+
+/*
+**head represents nb_bytes allocated on current page
+*/
+typedef struct		s_heap
+{
+	t_mem			*mem;
+	size_t			head;
+	size_t			stock;
+}					t_heap;
+
+typedef struct		s_data
+{
+	t_heap			tiny;
+	t_heap			small;
+	t_heap			large;
+	struct rlimit	limit;
+	size_t			total;
+}					t_data;
+
+/*
+**Malloc
+*/
+void				*malloc(size_t len);
+
+/*
+**Show alloc mem
+*/
+void				show_alloc_mem(void);
+t_data				*set_get_data(t_data *data);
+
+/*
+**Free
+*/
+void				free(void *ptr);
+
+/*
+**Realloc
+*/
+void				*ft_realloc(void *ptr, size_t size);
+
+#endif
---
+++ 42/malloc/hdezier/src/show_alloc_mem.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   show_alloc_mem.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdezier <hdezier@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/05 14:58:56 by hdezier           #+#    #+#             */
+/*   Updated: 2015/02/05 14:58:56 by hdezier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_malloc.h"
+#include <unistd.h>
+
+static void			ft_putnbr(unsigned long n, unsigned int base)
+{
+	const char		tab[] = "0123456789ABCDEF";
+	unsigned long	quo;
+	unsigned long	mod;
+
+	if (!n)
+		return ;
+	if (n > base - 1)
+	{
+		quo = n / base;
+		ft_putnbr(quo, base);
+		mod = n % base;
+		write(1, &tab[mod], 1);
+	}
+	else
+		write(1, &tab[n], 1);
+}
+
+static size_t		show_heap(t_heap *heap)
+{
+	t_mem	*current_mem;
+	size_t	result;
+
+	current_mem = heap->mem;
+	result = 0;
+	ft_putnbr((unsigned long)heap->mem, 16);
+	write (1, "\n", 1);
+	while (current_mem && (current_mem = current_mem->next))
+	{
+		if (current_mem->size == 0 || current_mem->free == 1)
+			continue ;
+		write (1, "0x", 3);
+		ft_putnbr((unsigned long)(current_mem + sizeof(t_mem)), 16);
+		write (1, " - 0x", 5);
+		ft_putnbr((unsigned long)(current_mem
+			+ sizeof(t_mem) + current_mem->size), 16);
+		write (1, " : ", 3);
+		ft_putnbr((unsigned long)(current_mem->size), 10);
+		result += current_mem->size;
+		write (1, " octets\n", 8);
+	}
+	return (result);
+}
+
+t_data				*set_get_data(t_data *data)
+{
+	static t_data	*data_save = NULL;
+
+	if (!data)
+		return (data_save);
+	data_save = data;
+	return (NULL);
+}
+
+void				show_alloc_mem(void)
+{
+	t_data			*data;
+	size_t			total;
+
+	if (!(data = set_get_data(NULL)))
+		return ;
+	total = 0;
+	if (data->tiny.mem && write(1, "TINY : 0x", 9))
+		total += show_heap(&(data->tiny));
+	if (data->small.mem && write(1, "SMALL : 0x", 11))
+		total += show_heap(&(data->small));
+	if (data->large.mem && write(1, "LARGE : 0x", 11))
+		total += show_heap(&(data->large));
+	write(1, "Total : ", 8);
+	if (total)
+		ft_putnbr((unsigned long)total, 10);
+	else
+		write(1, "0", 1);
+	write(1, " octets\n", 8);
+}
