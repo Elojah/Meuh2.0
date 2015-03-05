afdc965b14ff498bee41455c5360ea86970947f7
Hugo DEZIER <hdezier@e3r4p18.42.fr>
2015-03-05 14:14:44 +0100

    merge

--- 42/N-Puzzle/hdezier-erobert/src/Puzzle.cpp
+++ 42/N-Puzzle/hdezier-erobert/src/Puzzle.cpp
@@ -3,5 +3,36 @@
 Puzzle::Puzzle(void) {
 }
 
+Puzzle::Puzzle(std::istream &is) {
+	(void)is;
+}
+
 Puzzle::~Puzzle(void) {
 }
+
+void			Puzzle::parse(std::istream &is) {
+	(void)is;
+}
+
+void			Puzzle::move(char dir) {
+	tCase			tmp;
+	int				inc[2];
+
+	inc[0] = dir < 2 ? 0 : 1;
+	inc[1] = dir < 2 ? 1 : 0;
+	inc[0] *= dir % 2 ? 1 : -1;
+	inc[1] *= dir % 2 ? 1 : -1;
+	inc[0] += _empty[0];
+	inc[1] += _empty[1];
+	if (inc[0] >= 0 && inc[0] < static_cast<int>(_size)
+		&& inc[1] >= 0 && inc[1] < static_cast<int>(_size)) {
+		tmp = _map[EMPTY];
+		_map[EMPTY] = _map[INC];
+		_map[INC] = tmp;
+	}
+}
+
+void			Puzzle::resolve(void) {
+	;
+}
+
