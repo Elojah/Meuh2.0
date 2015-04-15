ffacbeecb25bc66180b6c4468a832c7575fa8ba7
Hugo DEZIER <hdezier@e3r4p20.42.fr>
2015-04-15 16:20:07 +0200

    lib ext done

--- 42/mod1/glfw_style/lib/glm/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /nfs/zfs-student-2/users/2013/hdezier/code/meuh/42/mod1/glfw_style/lib/glm
-# Build directory: /nfs/zfs-student-2/users/2013/hdezier/code/meuh/42/mod1/glfw_style/lib/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(glm)
-SUBDIRS(test)
-SUBDIRS(util)
