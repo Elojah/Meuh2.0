6fec5aed50aa81dcbd7f98014b8d6e61ab533f5a
Leeios <kellos@hotmail.fr>
2015-05-06 19:16:51 +0200

    rename Scop

---
+++ 42/Scop/hdezier/lib/libft/ft_lstdelone.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdezier <hdezier@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/23 14:24:25 by hdezier           #+#    #+#             */
+/*   Updated: 2014/03/26 10:36:09 by hdezier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	if (!alst)
+		return ;
+	if (*alst)
+	{
+		(*del)((*alst)->content, (*alst)->content_size);
+		free(*alst);
+		*alst = NULL;
+	}
+}
