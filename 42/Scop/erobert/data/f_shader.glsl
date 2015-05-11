01bdd1a46761afeb0a6f4386f4f6d851564fb457
Yyhrs <erobert@student.42.fr>
2015-05-11 16:21:14 +0200

    texture transition

--- 42/Scop/erobert/data/f_shader.glsl
+++ 42/Scop/erobert/data/f_shader.glsl
@@ -2,14 +2,19 @@
 
 uniform sampler2D texture_sampler;
 uniform int texture_on;
+uniform float var;
 out vec4 f_color;
 in float color;
 in vec2 uv;
 
-void	main(void)
+void		main(void)
 {
+	vec3	tmp;
+
+	tmp = vec3(color, color, color);
+	tmp *= 1 - var;		
 	if (texture_on == 1)
-		f_color = vec4(texture(texture_sampler, uv).rgb, 1.);
+		f_color = vec4(texture(texture_sampler, uv).rgb * var + tmp, 1.);
 	else
-		f_color = vec4(color, color, color, 1.);
+		f_color = vec4(texture(texture_sampler, uv).rgb * var + tmp, 1.);
 }
