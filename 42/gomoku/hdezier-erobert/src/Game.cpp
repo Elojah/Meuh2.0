7971ca9a6f2f41e9ce730c48686dd8e741b9b85a
Hugo DEZIER <hdezier@e3r4p18.42.fr>
2015-05-25 16:40:14 +0200

    gomoku

--- 42/gomoku/hdezier-erobert/src/Game.cpp
+++ 42/gomoku/hdezier-erobert/src/Game.cpp
@@ -1,4 +1,6 @@
 #include "Game.hpp"
+#include <ctime>
+#include <iostream>
 
 Game::Game(void) :
 	_gameOver(false) {
@@ -28,7 +30,31 @@ void		Game::launch(void) {
 }
 
 void		Game::loop(void) {
+	bool	turn(true);
+
 	while (!_gameOver && 0) {
-		;
+		if (turn) {
+			waitP1();
+		} else {
+			waitP2();
+		}
+		display();
+	}
+}
+
+void	Game::waitP1(void) {
+	;
+}
+
+void	Game::waitP2(void) {
+	;
+}
+
+void	Game::display(void) const {
+	for (int i = 0; i < SIZE_BOARD; ++i) {
+		for (int j = 0; j < SIZE_BOARD; ++j) {
+			std::cout << _board[i][j] << "\t";
+		}
+		std::cout << std::endl;
 	}
 }
