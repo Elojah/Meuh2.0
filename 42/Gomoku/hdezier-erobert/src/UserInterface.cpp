3f871a17ecd65e0567111a8143cf5146010c7845
Yyhrs <erobert@student.42.fr>
2015-05-31 16:18:01 +0200

    HELP button

--- 42/Gomoku/hdezier-erobert/src/UserInterface.cpp
+++ 42/Gomoku/hdezier-erobert/src/UserInterface.cpp
@@ -6,13 +6,14 @@
 //   By: erobert <erobert@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/05/28 12:13:37 by erobert           #+#    #+#             //
-//   Updated: 2015/05/31 15:24:15 by erobert          ###   ########.fr       //
+//   Updated: 2015/05/31 16:16:15 by erobert          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 #include "UserInterface.hpp"
 
-UserInterface::UserInterface(void)
+UserInterface::UserInterface(void):
+	_help(true)
 {
 	_font.loadFromFile("data/open_sans_light.ttf");
 	_text.setFont(_font);
@@ -42,10 +43,13 @@ void						UserInterface::render(Board const &b,
 	_window.clear(sf::Color(50, 100, 250));
 	renderBoard(b);
 	renderPlayers(p1, p2);
-	if (!p1.ai())
-		drawStone(p1.calculusMove().x, p1.calculusMove().y, HELP);
-	if (!p2.ai())
-		drawStone(p2.calculusMove().x, p2.calculusMove().y, HELP);
+	if (_help)
+	{
+		if (!p1.ai())
+			drawStone(p1.calculusMove().x, p1.calculusMove().y, P1_HELP);
+		if (!p2.ai())
+			drawStone(p2.calculusMove().x, p2.calculusMove().y, P2_HELP);
+	}
 	_window.display();
 }
 
@@ -55,27 +59,31 @@ UserInterface::sEvent const	&UserInterface::getEvent(void)
 	float					position[2];
 
 	_event.e = E_EVENT;
-	_window.waitEvent(event);
-	if (event.type == sf::Event::Closed 
-		|| (event.type == sf::Event::KeyPressed
-			&& event.key.code == sf::Keyboard::Escape))
-		_event.e = EXIT;
-	else if (event.type == sf::Event::MouseButtonPressed)
+	while (_window.pollEvent(event))
 	{
-		position[0] = sf::Mouse::getPosition(_window).x;
-		position[1] = sf::Mouse::getPosition(_window).y;
-		if (position[1] < HEIGHT)
+		if (event.type == sf::Event::Closed 
+			|| (event.type == sf::Event::KeyPressed
+				&& event.key.code == sf::Keyboard::Escape))
+			_event.e = EXIT;
+		else if (event.type == sf::Event::MouseButtonPressed)
 		{
-			_event.e = MOUSE;
-			_event.x = position[0] / ((10 + 52.05 * 19.) / _size);
-			_event.y = position[1] / ((10 + 52.05 * 19.) / _size);
-		}
-		else if (position[1] > HEIGHT + 12 && position[1] < HEIGHT + 52)
-		{
-			if (position[0] > 190 && position[0] < 290)
-				_event.e = P1_AI;
-			else if (position[0] < 810 && position[0] > 710)
-				_event.e = P2_AI;
+			position[0] = sf::Mouse::getPosition(_window).x;
+			position[1] = sf::Mouse::getPosition(_window).y;
+			if (position[1] < HEIGHT)
+			{
+				_event.e = MOUSE;
+				_event.x = position[0] / ((10 + 52.05 * 19.) / _size);
+				_event.y = position[1] / ((10 + 52.05 * 19.) / _size);
+			}
+			else if (position[1] > HEIGHT + 12 && position[1] < HEIGHT + 52)
+			{
+				if (position[0] > 190 && position[0] < 290)
+					_event.e = P1_AI;
+				else if (position[0] < 810 && position[0] > 710)
+					_event.e = P2_AI;
+				else if (position[0] < 600 && position[0] > 430)
+					_help = !_help;
+			}
 		}
 	}
 	return (_event);
@@ -108,8 +116,10 @@ void						UserInterface::initStone(void)
 	_stone[BLACK].setOutlineColor(sf::Color::Color(5, 5, 5, 100));
 	_stone[WHITE].setFillColor(sf::Color::White);
 	_stone[WHITE].setOutlineColor(sf::Color::Color(250, 250, 250, 100));
-	_stone[HELP].setFillColor(sf::Color::Color(5, 250, 5, 150));
-	_stone[HELP].setOutlineColor(sf::Color::Color(5, 250, 5, 100));
+	_stone[P1_HELP].setFillColor(sf::Color::Color(5, 250, 5, 150));
+	_stone[P1_HELP].setOutlineColor(sf::Color::Black);
+	_stone[P2_HELP].setFillColor(sf::Color::Color(5, 250, 5, 150));
+	_stone[P2_HELP].setOutlineColor(sf::Color::White);
 }
 void						UserInterface::drawStone(int x, int y,
 													 eStone stone)
@@ -141,32 +151,34 @@ void						UserInterface::renderBoard(Board const &b)
 void						UserInterface::renderPlayers(Player const &p1,
 														 Player const &p2)
 {
-	size_t					position;
-
 	_text.setCharacterSize(32);
 	_text.setStyle(sf::Text::Bold);
 	_text.setColor(sf::Color::Black);
 	_text.setPosition(64, HEIGHT + 12);
 	_text.setString("BLACK");
 	_window.draw(_text);
-	_text.setColor(sf::Color::Color(95, 200, 160));
+	_text.setColor(sf::Color::White);
+	_text.setString("WHITE");
+	_text.setPosition(WIDTH - 162, HEIGHT + 12);
+	_window.draw(_text);
+	_text.setColor(sf::Color::Color(205, 92, 92));
 	_text.setPosition(190, HEIGHT + 12);
 	if (p1.ai())
 		_text.setString("AI ON");
 	else
 		_text.setString("AI OFF");
 	_window.draw(_text);
-	_text.setColor(sf::Color::White);
-	position = WIDTH - 64 - _text.getLocalBounds().width;
-	_text.setPosition(position, HEIGHT + 12);
-	_text.setString("WHITE");
-	_window.draw(_text);
-	_text.setColor(sf::Color::Color(95, 200, 160));
 	if (p2.ai())
 		_text.setString("AI ON");
 	else
 		_text.setString("AI OFF");
-	position -= _text.getLocalBounds().width + 32;
-	_text.setPosition(position, HEIGHT + 12);
+	_text.setPosition(WIDTH - 291, HEIGHT + 12);
+	_window.draw(_text);
+	_text.setColor(sf::Color::Color(95, 200, 160));
+	if (_help)
+		_text.setString("HELP ON");
+	else
+		_text.setString("HELP OFF");
+	_text.setPosition(435, HEIGHT + 12);
 	_window.draw(_text);
 }
