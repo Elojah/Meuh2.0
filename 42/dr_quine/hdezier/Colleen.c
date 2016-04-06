df0163f00f135bb012e787bc3cea741fa343910b
Latias <kellos@hotmail.fr>
2016-04-06 23:15:10 +0200

    fix ex00

--- 42/dr_quine/hdezier/Colleen.c
+++ 42/dr_quine/hdezier/Colleen.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/*
+	COM_EXTERN
+*/
 int		main(void)
 {
-	char	*a="#include <stdio.h>%cint		main(void)%c{%c	char	*a=%c%s%c;%c	printf(a, 10, 10, 10, 34, a, 34, 10, 10, 10);%c}%c";
-	printf(a, 10, 10, 10, 34, a, 34, 10, 10, 10);
+	/*
+		COM_INTERN
+	*/
+	char	s[] = "#include <stdio.h>%c/*%c	COM_EXTERN%c*/%cint		main(void)%c{%c	/*%c		COM_INTERN%c	*/%c	char	s[] = %c%s%c;%c	printf(s, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10);%c}%c";
+	printf(s, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10);
 }
--- 42/dr_quine/hdezier/Grace.c
+++ 42/dr_quine/hdezier/Grace.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <fcntl.h>
-int		main(void)
-{
-	char	a[] = "#include <stdio.h>%c#include <fcntl.h>%cint		main(void)%c{%c	char	a[] = %c%s%c;%c	int		fd = open(%cGrace_kid.c%c, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);%c	dprintf(fd, a, 10, 10, 10, 10, 34, a, 34, 10, 34, 34, 10, 10, 10);%c}%c";
-	int		fd = open("Grace_kid.c", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-	dprintf(fd, a, 10, 10, 10, 10, 34, a, 34, 10, 34, 34, 10, 10, 10);
-}
+#define MAIN(x)\
+int		main(void)\
+{\
+	char	a[] = "#include <stdio.h>%c#include <fcntl.h>%cint		main(void)%c{%c	char	a[] = %c%s%c;%c	int		fd = open(%cGrace_kid.c%c, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);%c	dprintf(fd, a, 10, 10, 10, 10, 34, a, 34, 10, 34, 34, 10, 10, 10);%c}%c";\
+	int		fd = open("Grace_kid.c", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);\
+	dprintf(fd, a, 10, 10, 10, 10, 34, a, 34, 10, 34, 34, 10, 10, 10);\
+}\
+\
