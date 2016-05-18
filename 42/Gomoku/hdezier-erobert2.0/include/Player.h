ac0ecdd7fcf1d83fae7e23ff893bc2a38d06f3a3
Hugo DEZIER <hdezier@e2r8p20.42.fr>
2016-05-18 16:56:53 +0200

    ok propre

--- 42/Gomoku/hdezier-erobert2.0/include/Player.h
+++ 42/Gomoku/hdezier-erobert2.0/include/Player.h
@@ -6,7 +6,7 @@
 /*   By: hdezier <hdezier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/02 21:00:56 by hdezier           #+#    #+#             */
-/*   Updated: 2016/05/18 15:34:35 by hdezier          ###   ########.fr       */
+/*   Updated: 2016/05/18 16:50:29 by hdezier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,23 +34,21 @@ public:
 	inline void				switchAI() {m_ai = !m_ai;};
 	inline bool				ai(void) const {return (m_ai);};
 
-	common::vec2			play(const IBoard &board, const Rules &rules,
+	const common::vec2		play(const IBoard &board, const Rules &rules,
 								const common::vec2 &stroke, const common::eCell &player) const;
 
 	struct					sEval_P1 : public IEval
 	{
 		inline virtual uint8_t		eval(const IBoard &board, const Rules &rules, const sMinMaxState &minMaxState) const
 		{
-			uint8_t					result(112);
+			uint8_t					result;
 
 			const common::eCell win = rules.gameEnded(board, minMaxState.lastStroke, minMaxState.captures[0], minMaxState.captures[1]);
 			if (win == common::eCell::P2)
 				return (1 + (MAX_DEPTH - minMaxState.depth));
 			else if (win == common::eCell::P1)
 				return (255 - (MAX_DEPTH - minMaxState.depth));
-			int8_t align = board.countAllAlign(common::eCell::P1);
-			result += align;
-			result += (minMaxState.captures[0] - minMaxState.captures[1]);
+			result = 112 + board.countAllAlign(common::eCell::P1) + (minMaxState.captures[0] - minMaxState.captures[1]);
 			return (result);
 		}
 	};
@@ -59,23 +57,21 @@ public:
 	{
 		inline virtual uint8_t		eval(const IBoard &board, const Rules &rules, const sMinMaxState &minMaxState) const
 		{
-			uint8_t			result(112);
+			uint8_t			result;
 
 			const common::eCell win = rules.gameEnded(board, minMaxState.lastStroke, minMaxState.captures[0], minMaxState.captures[1]);
 			if (win == common::eCell::P1)
 				return (1 + (MAX_DEPTH - minMaxState.depth));
 			else if (win == common::eCell::P2)
 				return (255 - (MAX_DEPTH - minMaxState.depth));
-			int8_t align = board.countAllAlign(common::eCell::P2);
-			result += align;
-			result += (minMaxState.captures[1] - minMaxState.captures[0]);
+			result = 112 + board.countAllAlign(common::eCell::P2) + (minMaxState.captures[1] - minMaxState.captures[0]);
 			return (result);
 		}
 	};
 
 private:
 	bool					m_ai = false;
-	common::vec2			_calculusAI(const IBoard &board, const Rules &rules, const common::eCell &player) const;
+	const common::vec2			_calculusAI(const IBoard &board, const Rules &rules, const common::eCell &player) const;
 
 	sEval_P1		m_eval_P1;
 	sEval_P2		m_eval_P2;
--- 42/Gomoku/hdezier-erobert2.0/src/Board.cpp
+++ 42/Gomoku/hdezier-erobert2.0/src/Board.cpp
@@ -6,11 +6,105 @@
 /*   By: hdezier <hdezier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/02 20:47:04 by hdezier           #+#    #+#             */
-/*   Updated: 2016/05/02 22:56:28 by hdezier          ###   ########.fr       */
+/*   Updated: 2016/05/18 16:54:42 by hdezier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Board.h"
+
+template<uint8_t N>
+int8_t			Board<N>::countAllAlign(const common::eCell &player) const
+{
+	bool			markMap[N][N];
+	int8_t			result(0);
+
+	for (uint8_t i = 0; i < N; ++i)
+	{
+		for (uint8_t j = 0; j < N; ++j)
+			markMap[i][j] = false;
+	}
+
+	for (uint8_t i = 0; i < N; ++i)
+	{
+		for (uint8_t j = 0; j < N; ++j)
+		{
+			if (m_board[i][j] == common::eCell::NONE
+				|| markMap[i][j] == true)
+				continue ;
+			common::eCell	startCell(m_board[i][j]);
+			for (int8_t dir = -4; dir < 5; ++dir)
+			{
+				if (dir == 0)
+					continue ;
+				uint8_t			count(1);
+				uint8_t			n(0);
+				common::eCell	currentCell;
+				while (++n)
+				{
+					currentCell = getCell({i, j}, (common::eDirection)dir, n);
+					const auto	nextCellPosition = _convertCell({i, j}, (common::eDirection)dir, n);
+					if (!_isValid(nextCellPosition)
+						|| currentCell != startCell)
+						break ;
+					count += (n + 1);
+					markMap[nextCellPosition.x][nextCellPosition.y] = true;
+				}
+				if (dir > 0)
+				{
+					auto	alignFree = countAlignFree({i, j}, (common::eDirection)dir, startCell);
+					if (alignFree >= 2)
+					count += alignFree;
+				}
+				if (currentCell == common::eCell::NONE && count > 1)
+				{
+					count *= 2;
+					if (getCell({i, j}, (common::eDirection)(-dir), 1) == common::eCell::NONE)
+						count *= 2;
+				}
+				if (startCell == player)
+					result += count;
+				else
+					result -= count;
+			}
+		}
+	}
+	return (result);
+};
+
+template<uint8_t N>
+int8_t	Board<N>::_countAlignSide(const common::vec2 &stroke, const common::eDirection &dir, const common::eCell &player, bool &permissive) const
+{
+	int					n;
+
+	n = 0;
+	while (++n < N)
+	{
+		auto	cell = getCell(stroke, dir, n);
+		if (cell == player)
+			continue ;
+		else if (cell == common::eCell::NONE)
+		{
+			if (permissive && getCell(stroke, dir, n + 1) == player)
+			{
+				permissive = false;
+				auto	nextCount = _countAlignSide(_convertCell(stroke, dir, n), dir, player, permissive);
+				if (nextCount < 2)
+				{
+					permissive = true;
+					return (n - 1);
+				}
+				else
+					return (n + nextCount);
+			}
+			else
+				return (n - 1);
+		}
+		else // NONE || E_CELL (=out of range)
+			return (-1);
+	}
+	return (0);
+}
+
 
 template class Board<common::small>;
 template class Board<common::medium>;
